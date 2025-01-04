327bca20c1d8e9a5242a98af57c90d538c93e477
desbma <desbma@users.noreply.github.com>
2025-01-05 00:48:11 +0100

    feat: add changelog, debian package & man page generation + update ci

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 //! {{project_description}}
 
+#![cfg_attr(
+    feature = "gen-man-pages",
+    expect(dead_code, unused_crate_dependencies, unused_imports)
+)]
+
 use anyhow::Context as _;
 use clap::Parser as _;
 
 mod cl;
 mod config;
 
+#[cfg(feature = "gen-man-pages")]
+fn main() -> anyhow::Result<()> {
+    use clap::CommandFactory as _;
+    let cmd = cl::Args::command();
+    let output = std::env::args_os()
+        .nth(1)
+        .ok_or_else(|| anyhow::anyhow!("Missing output dir argument"))?;
+    clap_mangen::generate_to(cmd, output)?;
+    Ok(())
+}
+
+#[cfg(not(feature = "gen-man-pages"))]
 fn main() -> anyhow::Result<()> {
     // Init logger
     simple_logger::SimpleLogger::new()
